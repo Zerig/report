73d5b1581e05c16c5207a9833bba8f7300399248
Zerig <Zerig@users.noreply.github.com>
2020-04-28 23:30:38 +0200

    m

--- src/Report/Mysql.php
+++ src/Report/Mysql.php
@@ -6,29 +6,56 @@ class Mysql{
 
 
 	public static function set($mysql, $sql){
-		$errno = $mysql->errno;
-		$error = $mysql->error;
+		$errno 			= $mysql->errno;
+		$affected_rows 	= $mysql->affected_rows;
+		$state 			= self::getState($affected_rows);
+		$query_type 	= self::getQueryType($sql);
 
-		$msg = self::setMsg($errno);
+		$msg = self::setMsg($errno, $state, $query_type);
+
+		//$errno = ($errno == 0)? self::getQueryType($sql) : $errno;
+
+
 
 
 		$GLOBALS["report"][] = new rData([
-			"state" => "fail",
+			"state" => $state,
 			"errno" => $errno,
+			"rows"  => $affected_rows,
 			"msg"   => $msg
 		]);
 		return;
 
 	}
 
+	private function getQueryType($sql){
+		if(strpos($sql, 'INSERT') !== false) return "insert";
+		if(strpos($sql, 'UPDATE') !== false) return "update";
+		if(strpos($sql, 'DELETE') !== false) return "delete";
+		if(strpos($sql, 'SELECT') !== false) return "select";
 
-	private function setMsg($errno){
+		return "0";
+	}
+
+
+	private function getState($i){
+		if($i < 0)  return "fail";
+		if($i == 0) return "info";
+		if($i > 0)  return "success";
+	}
+
+
+	private function setMsg($errno, $state, $query_type){
 		// LOAD JSON DATA
 		$conf = \Noodlehaus\Config::load(__DIR__ . '\report.json');
 		$text = $conf->all();
 
+		//if($errno == 0)		$msg = $text["mysql"][$state][$query_type];
+		if($errno != 0)		$msg = $text["mysql"][$state][$errno];
+
+
 		// CHOOSE teh BEST JSON MSG
-		$msg = (isset($text["mysql"]["fail"][$errno]))? $text["mysql"]["fail"][$errno] : $text["mysql"]["fail"][0];
+		$msg = (isset($text["mysql"][$state][$errno]))? $text["mysql"][$state][$errno] : $text["mysql"][$state][0];
 		$num = substr_count($msg, "%s");
 		$len = count(\Report\Data::get());
 
